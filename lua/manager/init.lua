a989e14dbdca38b5339ed08b568488149f84038c
br0steen <osteensco@gmail.com>
2024-12-27 21:11:05 -0600

    type safety

--- lua/manager/init.lua
+++ lua/manager/init.lua
@@ -1,63 +1,98 @@
-local utils = require("utils")
--- manages of all shadow-clone windows and their buffers
+-- shadow clone manager
+
+
+---@class WinObj
+---@field bufnr number buffer number of a floating window
+---@field win number window ID of a floating window
+
+---@class WinGroup
+---@field windows table<WinObj>
+
+--- Manages all floating window buffers
+---@class Manager
+---@field stack table<WinGroup>
 local manager = {}
 
--- book keeping data structure
-manager.ledger = {
-    -- TODO
-    --  - merge terminal and window objects
-    --  - convert to a stack, removes the need for last_accessed
-    --  - handle window groups
-    float = {
-        terminal = {
-            array = {},
-            last_accessed = { win = -1, term = -1 },
-        },
-        window = {
-            array = {},
-            last_accessed = { win = -1, term = -1 },
-        }
-    },
-    normal = {
-        terminal = {
-            array = {},
-            last_accessed = { win = -1, term = -1 },
-        },
-        window = {
-            array = {},
-            last_accessed = { win = -1, term = -1 },
-        }
-    },
-}
+-- Manager's main stack containing floating windows.
+-- Floating windows are always part of a group, the stack represents the group's relative position on the z axis.
+-- There are some assumptions with the stack:
+--  - The stack should roughly correspond to the zindex of a window. The top of the stack having the highest z index.
+--  -
+manager.stack = {}
+-- TODO
+--  - Handle hidden buffers.
+
 
 -- methods
-manager.get_len = function(win_type, buf_type)
-    return #manager.ledger[win_type][buf_type].array
+
+---get length of the manager's stack
+---@return number
+manager.get_len = function()
+    return #manager.stack
 end
 
-manager.update_access = function(win)
-    local win_type, buf_type = utils.get_types(win)
-    manager.ledger[win_type][buf_type].last_accessed = win
+---push a group onto manager's stack
+---@param group WinGroup
+manager.push = function(group)
+    local length = manager.get_len()
+    manager.stack[length + 1] = group
 end
 
-manager.append = function(win)
-    local win_type, buf_type = utils.get_types(win)
-    local length = manager.get_len(win_type, buf_type)
-    manager.ledger[win_type][buf_type].array[length + 1] = win
-    manager.update_access(win)
+---pop a group off the manager's stack
+---@return WinGroup
+manager.pop = function()
+    local length = manager.get_len()
+    local group = manager.stack[length]
+    table.remove(manager.stack, length)
+    return group
 end
 
-manager.remove = function()
+---peak at the top of the manager's stack
+---@return WinGroup
+manager.peak = function()
+    local length = manager.get_len()
+    if length <= 1 then
+        return {}
+    end
+    return manager.stack[length]
+end
+
+---swaps the group on top of the stack with the group directly below
+manager.swap = function()
+    local length = manager.get_len()
+    if length <= 1 then
+        return
+    end
+    local top = manager.stack[length]
+    local bott = manager.stack[length - 1]
+    manager.stack[length] = bott
+    manager.stack[length - 1] = top
+end
+
+---adds window object to a group
+---@param group WinGroup
+---@param window WinObj
+manager.add_to_group = function(group, window)
+    group = group or {}
+    table.insert(group, window)
+end
+
+---removes a window object from a group
+---@param group WinGroup
+---@param window WinObj
+manager.remove_from_group = function(group, window)
+    for pos, win in ipairs(group) do
+        if win.bufnr == window.bufnr and win.win == window.win then
+            table.remove(group, pos)
+            return
+        end
+    end
     -- TODO
+    --  - handle not finding the window object in the group
 end
 
-manager.set_win_type = function(win)
-    -- TODO
-end
 
-manager.set_buf_type = function(win)
-    -- TODO
-end
+
 
 
 
--- lua/shadow-clone/init.lua
+++ lua/shadow-clone/init.lua
@@ -5,6 +5,14 @@ local manager = require("manager")
 local M = {}
 
 -- defaults
+
+---@class FloatConfig Float window settings.
+---@field position string String representing how the window is anchored (center, left, right, top, bottom).
+---@field width number
+---@field height number
+
+---@class Config Settings passed to setup function that shadow-clone.nvim uses.
+---@field float_window? FloatConfig
 M.config = {
     float_window = {
         position = 'center',
@@ -13,21 +21,22 @@ M.config = {
         width = math.ceil(math.min(vim.o.columns, math.max(80, vim.o.columns - 10))),
         height = math.ceil(math.min(vim.o.lines, math.max(20, vim.o.lines - 5))),
     },
-    normal_window = {},
 }
 
 
 
 -- public methods
 
--- Creates a floating window and adds it to shadow-clone.nvim's window manager. Uses a default config
--- if none is passed in, and creates a new buffer if an existing one is not provided.
---
--- args:
---  - opts: optionally provide you're own win_config to be passed to vim.api.nvim_open_win,
---    and/or buffer to be reused. For win_config see `vim.api.keyset.win_config`.
---
---    opts = { win_config = {}, buf = -1 }
+---@class CreateFloatOpts
+---@field win_config? vim.api.keyset.win_config
+---@field config? FloatConfig
+---@field buf? number
+---@field newgroup boolean
+
+--- Creates a floating window and adds it to shadow-clone.nvim's window manager.
+--- Uses a default config if none is passed in, and creates a new buffer if an existing one is not provided.
+---@param opts? CreateFloatOpts Optionally provide you're own win_config to be passed to vim.api.nvim_open_win, and/or buffer to be reused. For win_config see `vim.api.keyset.win_config`.
+---@return WinObj
 M.create_floating_window = function(opts)
     opts = opts or {}
     opts.win_config = opts.win_config or {}
@@ -62,31 +71,36 @@ M.create_floating_window = function(opts)
 
     -- window obj
     local window = {
-        buf = buf,
+        bufnr = buf,
         win = vim.api.nvim_open_win(buf, true, win_config)
     }
 
     -- add window to shadow-clone.nvim's manager
-    manager.append(window)
+    local group = {}
+    if not opts.newgroup then
+        group = manager.pop()
+    end
+    manager.add_to_group(group, window)
+    manager.push(group)
 
     return window
 end
 
 -- toggle a floating terminal, creates a new terminal buffer if float.term.last_accessed is nil
-M.toggle_floating_terminal = function()
-    local term = manager.ledger.float.terminal.last_accessed
-    if not vim.api.nvim_win_is_valid(term.win) then
-        term = M.create_floating_window({ buf = term.buf })
-        -- TODO
-        --  - abstract into "terminal start" function
-        if vim.bo[term.buf].buftype ~= "terminal" then
-            vim.cmd.terminal()
-        end
-        manager.update_access(term)
-    else
-        vim.api.nvim_win_hide(term.win)
-    end
-end
+-- M.toggle_floating_terminal = function()
+--     local term = manager.ledger.float.terminal.last_accessed
+--     if not vim.api.nvim_win_is_valid(term.win) then
+--         term = M.create_floating_window({ buf = term.buf })
+--         -- TODO
+--         --  - abstract into "terminal start" function
+--         if vim.bo[term.buf].buftype ~= "terminal" then
+--             vim.cmd.terminal()
+--         end
+--         manager.update_access(term)
+--     else
+--         vim.api.nvim_win_hide(term.win)
+--     end
+-- end
 
 -- move current buffer into a new floating window (normal -> floating)
 M.bubble_up = function()
@@ -95,7 +109,7 @@ M.bubble_up = function()
     local win_type = vim.fn.win_gettype(win)
     if win_type ~= "popup" then
         vim.api.nvim_win_close(win, true)
-        M.create_floating_window({ buf = buf })
+        M.create_floating_window({ buf = buf, newgroup = true })
     end
 end
 
@@ -106,6 +120,8 @@ M.bubble_down = function()
     local win_type = vim.fn.win_gettype(win)
     if win_type == "popup" then
         vim.api.nvim_win_close(win, true)
+        local group = manager.peak()
+        manager.remove_from_group(group, { bufnr = buf, win = win })
         vim.api.nvim_set_current_buf(buf)
     end
 end
@@ -117,6 +133,8 @@ M.bubble_down_h = function()
     local win_type = vim.fn.win_gettype(win)
     if win_type == "popup" then
         vim.api.nvim_win_close(win, true)
+        local group = manager.peak()
+        manager.remove_from_group(group, { bufnr = buf, win = win })
         vim.cmd("split")
         vim.api.nvim_set_current_buf(buf)
     end
@@ -129,25 +147,32 @@ M.bubble_down_v = function()
     local win_type = vim.fn.win_gettype(win)
     if win_type == "popup" then
         vim.api.nvim_win_close(win, true)
+        local group = manager.peak()
+        manager.remove_from_group(group, { bufnr = buf, win = win })
         vim.cmd("vsplit")
         vim.api.nvim_set_current_buf(buf)
     end
 end
--- hide current floating window
-M.hide_floating_window = function()
-    local win = vim.api.nvim_get_current_win()
-    vim.api.nvim_win_hide(win)
-end
 
--- toggle last accessed window
-M.toggle_last_accessed_win = function()
-    local win = manager.ledger.float.window.last_accessed
-    if not vim.api.nvim_win_is_valid(win.win) then
-        win = M.create_floating_window({ buf = win.buf })
-    else
-        M.hide_floating_window()
-    end
-end
+--TODO
+-- - rework hide and toggle functions
+-- - how to handle hidden buffers (hidden stack?)
+
+-- hide current floating window
+-- M.hide_floating_window = function()
+--     local win = vim.api.nvim_get_current_win()
+--     vim.api.nvim_win_hide(win)
+-- end
+--
+-- -- toggle last accessed window
+-- M.toggle_last_accessed_win = function()
+--     local win = manager.ledger.float.window.last_accessed
+--     if not vim.api.nvim_win_is_valid(win.win) then
+--         win = M.create_floating_window({ buf = win.buf })
+--     else
+--         M.hide_floating_window()
+--     end
+-- end
 
 -- split floating window horizontally
 M.h_split = function()
@@ -173,7 +198,8 @@ M.h_split = function()
             width = win_w,
             row = anchor[1],
             col = anchor[2]
-        }
+        },
+        newgroup = false
     })
     M.create_floating_window({
         buf = buf,
@@ -182,7 +208,8 @@ M.h_split = function()
             width = win_w,
             row = anchor[1] + math.floor(win_h / 2) + 1,
             col = anchor[2]
-        }
+        },
+        newgroup = false
     })
 end
 
@@ -210,7 +237,8 @@ M.v_split = function()
             width = math.floor(win_w / 2) - 1,
             row = anchor[1],
             col = anchor[2]
-        }
+        },
+        newgroup = false
     })
     M.create_floating_window({
         buf = buf,
@@ -219,7 +247,8 @@ M.v_split = function()
             width = math.floor(win_w / 2),
             row = anchor[1],
             col = anchor[2] + math.floor(win_w / 2) + 1
-        }
+        },
+        newgroup = false
     })
 end
 
@@ -233,26 +262,26 @@ end
 
 -- commands
 vim.api.nvim_create_user_command("SCwindow", M.create_floating_window, { nargs = '?' })
-vim.api.nvim_create_user_command("SCtoggleterm", M.toggle_floating_terminal, { nargs = 0 })
+-- vim.api.nvim_create_user_command("SCtoggleterm", M.toggle_floating_terminal, { nargs = 0 })
 vim.api.nvim_create_user_command("SCbubbleup", M.bubble_up, { nargs = 0 })
 vim.api.nvim_create_user_command("SCbubbledown", M.bubble_down, { nargs = 0 })
 vim.api.nvim_create_user_command("SCbubbledownh", M.bubble_down_h, { nargs = 0 })
 vim.api.nvim_create_user_command("SCbubbledownv", M.bubble_down_v, { nargs = 0 })
-vim.api.nvim_create_user_command("SChide", M.hide_floating_window, { nargs = 0 })
-vim.api.nvim_create_user_command("SCtoggle", M.toggle_last_accessed_win, { nargs = 0 })
+-- vim.api.nvim_create_user_command("SChide", M.hide_floating_window, { nargs = 0 })
+-- vim.api.nvim_create_user_command("SCtoggle", M.toggle_last_accessed_win, { nargs = 0 })
 vim.api.nvim_create_user_command("SCsplit", M.h_split, { nargs = '?' })
 vim.api.nvim_create_user_command("SCvsplit", M.v_split, { nargs = '?' })
 
 
+
 -- setup function
+---@param config Config
 M.setup = function(config)
     config = config or {}
     M.config = vim.tbl_deep_extend('force', M.config, config)
 end
 
--- TODO REMOVE ME!!!!!
--- M.create_floating_window()
--- print(vim.inspect(M.manager.float[1]))
+
 
 
 return M
